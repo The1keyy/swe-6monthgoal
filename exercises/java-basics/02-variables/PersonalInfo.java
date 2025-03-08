6b03600f33e0cc485ebeb94c9839f2a89bb63030
key <keyshawn.jeannot001@umb.edu>
2025-03-08 14:48:02 -0500

    add ed the lesson for basic java and variable. this included adding file for them both for exersise

---
+++ exercises/java-basics/02-variables/PersonalInfo.java
@@ -0,0 +1,25 @@
+/*Define these variables:
+
+        name (String)
+        age (int)
+        isStudent (boolean)
+        gpa (double)
+        initial (char)
+3️⃣ Print all variables using System.out.println()*/
+
+public class PersonalInfo { 
+    public static void main (String[] args){
+        String name = "Keyshawn";
+        int age = 22;
+        boolean isStudent = true;
+        double gpa = 5.55;
+        char firstLetter = 'K';
+        System.out.println("My name is " + name + " and im " + age + ". Its " + isStudent + " that im student. Also i hold a " + gpa + ". Also the first letter in my name start with the letter "+ firstLetter);
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(isStudent);
+        System.out.println(gpa);
+        System.out.println(firstLetter);
+    }
+}
+
