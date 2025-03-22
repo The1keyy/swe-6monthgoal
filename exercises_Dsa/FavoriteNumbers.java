6f352fedcbd39b0d582f5a7b86d4d47d22e85f98
key <keyshawnjeannot2@gmail.com>
2025-03-22 16:42:07 -0400

    add array dsa

---
+++ exercises_Dsa/FavoriteNumbers.java
@@ -0,0 +1,44 @@
+/**
+ Create a class called FavoriteNumbers.java
+Inside:
+
+Declare an int[] array of 3 elements
+
+Assign your 3 favorite numbers
+
+Print them all using System.out.println()
+ 
+
+public class FavoriteNumbers {
+    public static void main(String[] args){
+        int[] favorite = new int[3];
+
+        favorite[0] = 5;
+        favorite[1] = 2;
+        favorite[2] = 1;
+
+        System.out.println( favorite[0]);
+        System.out.println( favorite[1]);
+        System.out.println( favorite[2]);
+    }
+}
+    
+
+Calculate the sum of the three numbers
+
+Print the result with a message like:
+
+The sum of my favorite numbers is: 8 */
+
+public class FavoriteNumbers {
+    public static void main (String[] args) {
+        int[] favorite = new int[3];
+
+        favorite[0] = 5;
+        favorite[1] = 2;
+        favorite[2] = 1;
+        
+        System.out.println("The sum of my favorite numbers is: " + ( favorite[0] + favorite[1] + favorite[2]));
+
+    }
+}
