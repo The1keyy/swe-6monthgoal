73e51149c4f9d9e5bcf765a6c5533098d1fd2f14
key <keyshawnjeannot2@gmail.com>
2025-03-23 19:38:39 -0400

    finsh exerise

---
+++ exercises_Dsa/FavoriteQuote.java
@@ -0,0 +1,49 @@
+/*
+ Create a new Java class called FavoriteQuote
+
+Inside the main method, do the following:
+
+Create a String variable named quote
+
+Assign it a sentence like:
+"Success is not final, failure is not fatal."
+
+Print it using System.out.println(quote);
+ 
+
+public class FavoriteQuote{
+    public static void main(String [] args){
+        String quote = "Success is not final, failure is not fatal";
+        System.out.println(quote);
+    }
+    
+}
+
+String Exercise: Analyze the Quote
+ Your Task:
+Given this string:
+
+String quote = "Success is not final, failure is not fatal";
+ Write a Java program that prints:
+
+The full quote
+
+The length of the string
+
+The first character
+
+The last character
+
+
+    
+public class FavoriteQuote{
+    public static void main (String [] args){
+        String quote = "Success is not final, failure is not fatal";
+        System.out.println(quote);
+        System.out.println("length: " + quote.length());
+        System.out.println("The first character " + quote.charAt(0));
+        System.out.println("The last character " + quote.charAt(quote.length() - 1));
+    }
+}
+
+*/
