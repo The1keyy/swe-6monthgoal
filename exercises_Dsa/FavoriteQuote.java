6da5dbfa79cf99c6556e1836dac404509f38337a
key <keyshawnjeannot2@gmail.com>
2025-03-24 21:13:39 -0400

    add another exersie equaution

--- exercises_Dsa/FavoriteQuote.java
+++ exercises_Dsa/FavoriteQuote.java
@@ -75,10 +75,50 @@ Extracts and prints the word "failure"
 Extracts and prints the last word "fatal"
 
 
- */
 
- public class FavoriteNumbers {
+
+ public class FavoriteQuote{
     public static void main(String [] args){
-        
+        String quote = "Success is not final, failure is not fatal";
+
+        String word1 = quote.substring(0, 7);
+        String word2 = quote.substring(22, 29);
+        String word3 = quote.substring(37,42 );
+
+        System.out.println(word1 + " " + word2 + " " + word3);
     }
  }
+
+Write a program that:
+
+Loops through the string backwards
+
+Builds and prints the reversed string:
+
+
+
+public class FavoriteQuote{
+    public static void main(String [] args){
+        String name = "Keyshawn";
+        String reverse = "";
+
+        for( int i = name.length() - 1 ; i >= 0 ; i -- ){
+        reverse += name.charAt(i);
+        }
+        System.out.println("Reverse: " + reverse);
+    }
+}
+
+Write a Java program that:
+
+Stores a word (like "madam" or "racecar")
+
+Reverses the string (like you just did before)
+
+Compares the original and reversed string using .equals()
+
+Prints whether it is or is not a palindrome
+
+
+*/
+
